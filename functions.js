5031fa2330de07e4c2549d1bb48d0d9aed84c0a7
hk1510 <hkthetinkerer@gmail.com>
2018-02-06 21:28:42 -0800

    Added different pages

---
+++ functions.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+
+}
+
+function imghover() {
+  document.getElementById('buttonImg').setAttribute('src', 'Images/arrowDown.png');
+  console.log('hover');
+}
+function imgunhover() {
+  document.getElementById('buttonImg').setAttribute('src', 'Images/arrowDownWhite.png');
+  console.log('unhover');
+}
