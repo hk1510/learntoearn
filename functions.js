59de315039e6c5fff0c79079ee376a397ccb850b
hk1510 <hkthetinkerer@gmail.com>
2018-02-10 14:58:46 -0800

    used backstretch

--- functions.js
+++ functions.js
@@ -1,12 +1,21 @@
 window.onload = function(){
 
-}
+  function imghover() {
+    document.getElementById('buttonImg').setAttribute('src', 'Images/arrowDown.png');
+    console.log('hover');
+  }
+  function imgunhover() {
+    document.getElementById('buttonImg').setAttribute('src', 'Images/arrowDownWhite.png');
+    console.log('unhover');
+  }
 
-function imghover() {
-  document.getElementById('buttonImg').setAttribute('src', 'Images/arrowDown.png');
-  console.log('hover');
-}
-function imgunhover() {
-  document.getElementById('buttonImg').setAttribute('src', 'Images/arrowDownWhite.png');
-  console.log('unhover');
+   $("#secondbackground").backstretch("https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/HPOKFbR/female-hands-typing-computer-keyboard-using-desktop-computers-for-work-or-study-1920x1080-1080p-hd-format_mjyb9w38__F0000.png");
+
+   $("#thirdbackground").backstretch("https://mcgeepharma.com/wp-content/uploads/2017/07/13-13-things-personal-organizers-wont-tell-you-papers.jpg");
+
+   $("#fourthbackground").backstretch("https://wallpaperscraft.com/image/paper_colored_stack_11151_1920x1080.jpg");
+
+   $("#fifthbackground").backstretch("https://media.vogue.in/wp-content/uploads/2017/08/How-to-organise-your-closet.jpg");
+
+   $("#sixthbackground").backstretch("http://cdn.skim.gs/images/v1/msi/zvsxn8cy52k2dfj0elhm/sneaky-ways-illegal-interview-questions-are-rephrased");
 }
